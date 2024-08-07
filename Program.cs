0e5a4a8980e4a7f53c2b99a72f5fcb79c02e11ad
Alptekin Küçükali <100528474+Akali61@users.noreply.github.com>
2024-08-07 15:20:11 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// Compare Operators
+
+int a = 61, b = 5, c = 10, d = 3;
+string username = "Alptekin Küçükali";
+string password = "1234";
+
+var result = (a == b); //True
+result = (c == d);
+result = (username == "Alptekin Küçükali"); //True
+result = (password == "12345"); //False
+
+result = (a != b);
+result = (a > c);
+result = (a < c);
+result = (a >= b);
+
+    //Ternary
+var result2 = (a > b) ? "a büyük": (a == b) ? "a ve b eşit.": "b büyük";
+
+Console.WriteLine(result2);
