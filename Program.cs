15b1d9e9049e6d226fc67c7461a29c0d62ee43ed
Alptekin Küçükali <100528474+Akali61@users.noreply.github.com>
2024-08-07 14:58:34 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// Assignment Operators
+
+var a = 5;
+var b = 10;
+
+a += b;
+a += 5;
+a -= b;
+a *= b;
+a /= b;
+a %= b;
+
+//Math
+double result;
+
+result = Math.Pow(2,3);
+result = Math.Sqrt(25);
+result = Math.Abs(-10);
+result = Math.Round(4.5);
+result = Math.Ceiling(4.8);
+result = Math.Max(10,20);
+result = Math.Min(42,61);
+
+Console.WriteLine(result);
