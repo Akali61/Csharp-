2b8451cc90b3f95971eb3af4e20a93546539ae17
Alptekin Küçükali <100528474+Akali61@users.noreply.github.com>
2024-08-07 12:32:28 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿//Arithmetic Operators
+
+var a = 10;
+var b = 20;
+var c = 30;
+
+//var result = a+b;
+//var result = a-b;
+//var result = a*b;
+//var result = (float)a/(float)b;
+//var result = (a+b)*c;
+//var result = a % b;
+//var result = 14%2;
+
+var result = ++a;
+
+Console.WriteLine(result);
