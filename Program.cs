8f44008297ed22a7221520cc1ccd093fa6c67e63
Alptekin Küçükali <100528474+Akali61@users.noreply.github.com>
2024-08-07 14:15:31 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿//Arithmetic Operators Practice
+
+//1- a=10 , b=5 , c=20 ise c-a farkının b katı kaçtır?
+
+//2- int? a = 50; int b = 20 ise a+b değerini bulunuz.
+
+//3- a=10, b=20 ise a=b--; atamasından sonra a ve b değerlerine ne olur?
+
+//4- Klavyeden girilen bir sayının tek mi çift mi olduğunu söyleyen program.
+
+
+//int a = 10;
+//int b = 5;
+//int c = 20;                           1
+//var result = (c-a) * b;
+//Console.WriteLine(result);
+
+
+//int? a = null;
+//int b = 20;
+//                                      2
+//var result = (a ?? 0) + b;
+//Console.WriteLine(result);
+
+
+//int a = 10;
+//int b = 20;
+//                                      3
+//a = b--;
+//Console.WriteLine(a);
+//Console.WriteLine(b);
+
+
+//Console.Write("Number: ");
+//int Number = int.Parse(Console.ReadLine() ?? "0");
+//var result = Number % 2;              4
+
+//Console.WriteLine(result); // if result is 1, odd number; if result is 2, even number.
