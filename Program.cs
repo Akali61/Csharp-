dd0d990e73d569c34238516bb5bce1b120c85f38
Alptekin Küçükali <100528474+Akali61@users.noreply.github.com>
2024-08-08 13:38:12 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// If Else Block
+
+string username = "Alptekin";
+string password = "12345674";
+
+if (username == "Alptekin") {
+
+    if (password == "12345") {
+        Console.WriteLine("Hello");
+    } else {
+        Console.WriteLine("The password is not correct.");
+    }
+}
+else {
+    Console.WriteLine("The username is incorrect.");
+}
