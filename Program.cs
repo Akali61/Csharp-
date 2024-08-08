1fffa77d6cef4c8f57e1a82c7ebe8bd5b977c8e4
Alptekin Küçükali <100528474+Akali61@users.noreply.github.com>
2024-08-08 14:16:24 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿// If Else Practice
+Console.WriteLine("Toplama için: +");
+Console.WriteLine("Çıkartmak için: -");
+Console.WriteLine("Çarpmak için: *");
+Console.WriteLine("Bölmek için: /");
+
+Console.WriteLine("Seçiniz: ");
+var secim = Console.ReadLine();
+
+Console.Write("1. Sayı: ");
+var sayi1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("2. Sayı: ");
+var sayi2 = Convert.ToInt32(Console.ReadLine());
+
+double sonuc = 0;
+if (secim == "+") {
+    //Toplama
+    sonuc = sayi1 + sayi2;
+    Console.WriteLine(sonuc);
+} else if (secim == "-") {
+    //Çıkartma
+    sonuc = sayi1 - sayi2;
+    Console.WriteLine(sonuc);
+} else if (secim == "*") {
+    //Çarpma
+    sonuc = sayi1 * sayi2;
+    Console.WriteLine(sonuc);
+} else if (secim == "/"){
+    //Bölme
+    sonuc = sayi1 / sayi2;
+    Console.WriteLine(sonuc);  
+} else {
+    Console.WriteLine("Hatalı seçim.");
+}
