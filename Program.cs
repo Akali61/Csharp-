b88aaf6e993304adac08725d680573846073d7bf
Alptekin Küçükali <100528474+Akali61@users.noreply.github.com>
2024-08-08 13:49:30 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// If Elseif Block
+
+//string username = "Alptekin";
+//string password = "12345674";
+
+//if (username != "Alptekin") {
+//    Console.WriteLine("Username is wrong.");
+//    } else if (password !="12345674") {
+//        Console.WriteLine("Password is wrong.");
+//   }
+
+//else {
+//    Console.WriteLine("Hello");
+//}
+
+
+int x = 20;
+int y = 23;
+
+if (x > y) {
+    Console.WriteLine("x is bigger than y");
+} 
+else if (x == y) {
+    Console.WriteLine("x is equal y");
+}
+else {
+    Console.WriteLine("y is bigger than x");
+}
