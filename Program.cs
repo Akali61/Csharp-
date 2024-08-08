bc4bdde4ca7cb7756c902f7c562aa9cd273cd6d7
Alptekin Küçükali <100528474+Akali61@users.noreply.github.com>
2024-08-08 10:38:04 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// Logical Operators
+
+var a = true;
+var b = true;
+var c = false;
+var d = false;
+
+// 1- And - &&
+
+var result = (a && b);
+result = (a && c);
+result = (c && d);
+
+// 2- Or - ||
+
+result = (a || b);
+result = (c || b);
+result = (c || d);
+
+// 3- Not - !
+
+result = !c;
+Console.WriteLine(result);
