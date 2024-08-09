01f208ad5b9c660ac565c20cc0d599ec8e8dbc7b
Alptekin Küçükali <100528474+Akali61@users.noreply.github.com>
2024-08-09 14:01:45 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// Break & Continue
+
+// string name = "Alptekin Küçükali";
+
+// for (var i = 0; i < name.Length; i++)
+// {
+//     if (name[i] == 'i')
+//         continue;
+    
+//     Console.WriteLine(name[i]);
+// }
+// Console.WriteLine("The loops end.");
+
+
+int x = 0;
+while(x < 5) {
+    x++;
+    if (x == 3)
+        continue;
+    Console.WriteLine(x);
+}
