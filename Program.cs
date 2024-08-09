2838dcee0d1e33402dec5d6c96025d47915bbc66
Alptekin Küçükali <100528474+Akali61@users.noreply.github.com>
2024-08-09 11:46:41 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿// While
+
+// for (var i = 0; i < 10; i++)
+// {
+//     Console.WriteLine(i);
+// }
+
+// var i = 0;
+
+// while i < 10 {
+//     Console.WriteLine(i);
+//     i++;
+// }
+
+// string[] names = {"Ali", "Ahmet", "Canan"};
+
+// var i = 0;
+// while (i < names.Length)
+// {
+//     Console.WriteLine(names[i]);
+//     i++;
+// }
+
+var choose = "y";
+var counter = 1;
+var total = 0;
+
+while (choose == "y") {
+    Console.Write($"{counter}. number: ");
+    total += Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("You want to continue? yes or no : y/n: ");
+    choose = Console.ReadLine();
+
+    counter++;
+}
+
+Console.WriteLine($"{counter-1} equal is plus the numbers: {total}");
