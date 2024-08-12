0aad16bb67d13ebf71b36fb32759233d1bc61b69
Alptekin Küçükali <100528474+Akali61@users.noreply.github.com>
2024-08-12 10:27:02 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// Do-While Loop
+
+// int i = 1;
+// do {
+//     Console.WriteLine(i);
+//     i++;
+// } while (false);
+
+Console.Write("Piece: ");
+
+int piece = Convert.ToInt32(Console.ReadLine());
+
+string [] products = new string[piece];
+int i = 0;
+
+do {
+    Console.Write("Product Name: ");
+    products[i] = Console.ReadLine() ?? "";
+
+    i++;
+} while (piece != i);
+
+Console.WriteLine("Products are listed...");
+
+for (var a = 0; a < products.Length; a++) {
+    Console.WriteLine(products[a]);
+}
