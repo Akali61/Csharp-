ede7702cb0ff8bd1fbd5702f9ca3ed4f2540dea7
Alptekin Küçükali <100528474+Akali61@users.noreply.github.com>
2024-08-12 10:51:55 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿//Foreach
+
+string name =  "Alptekin";
+
+// for (var i = 0; i < name.Length; i++) {
+//     Console.WriteLine(name[i]);
+// }
+
+foreach (var letter in name) {
+    Console.WriteLine(letter);
+}
+
+int [] numbers = {10,20,40,45,50};
+foreach (var number in numbers){
+    Console.WriteLine(number);
+}
