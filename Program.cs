8628d9176fd1438a8cd616e197fa0267ff50709c
Alptekin Küçükali <100528474+Akali61@users.noreply.github.com>
2024-08-12 13:06:45 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// Directory.CreateDirectory("temp");
+// Directory.CreateDirectory("temp/deneme");
+// Directory.CreateDirectory("temp/deneme2");
+
+if(Directory.Exists("temp/deneme2")) {
+    Directory.Delete("temp/deneme");
+} else {
+    Console.WriteLine ("There is no folder you want to delete.");
+}
