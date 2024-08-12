8404a7f64c59d837f9940bda5631caa0f8a8c2a1
Alptekin Küçükali <100528474+Akali61@users.noreply.github.com>
2024-08-12 13:25:50 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// string path = @"C: \ temp";
+// string path = "\Users\alptekinkucukali\Desktop\temp\deneme";
+
+string rootPath = Directory.GetCurrentDirectory();
+
+string[] dirs = Directory.GetDirectories(rootPath,"*", SearchOption.AllDirectories);
+
+foreach (var dir in dirs){
+    Console.WriteLine(dir);
+}
+
+string [] files = Directory.GetFiles(rootPath, "*.txt", SearchOption.AllDirectories);
+
+foreach (var file in files){
+    Console.WriteLine(file);
+    Console.WriteLine(Path.GetExtension(file));
+    Console.WriteLine(Path.GetFileNameWithoutExtension(file));
+    Console.WriteLine(Path.GetFileName(file));
+}
