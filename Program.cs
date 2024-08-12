41e1f50b01e6f903b62bffe6f9cacd0f2acd1092
Alptekin Küçükali <100528474+Akali61@users.noreply.github.com>
2024-08-12 14:06:56 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp {
+
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Merhaba Alptekin");
+
+            // Array
+
+            //Collections
+
+            // => Non genetic collections
+            // => ArrayList => int,string
+
+            //Genetic collections
+            // => int, string
+        }
+    }
+
+    class Student {
+
+    }       
+}
+
+    
