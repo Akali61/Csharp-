ca732b50dd4f486aa9b343fae9147402e568c53c
Alptekin Küçükali <100528474+Akali61@users.noreply.github.com>
2024-08-12 14:24:50 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// Excepiton
+//System.FormatException
+//System.DivideByZeroException
+//System.NullReferenceException  
+
+using System.Net.Http.Headers;
+
+Console.Write("1. Number: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("2. Number: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+var result = number1 / number2;
+
+prcdtvada prcdtvada = null;
+Console.WriteLine(prcdtvada.prcdtvadaname)
+
+//Exception Handling
